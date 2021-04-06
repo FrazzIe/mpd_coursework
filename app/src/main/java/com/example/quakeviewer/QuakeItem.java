59bbef5f3499c4d99a6350f6569208c1b0e1cc3a
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-04-06 19:48:46 +0100

    split up depth

--- app/src/main/java/com/example/quakeviewer/QuakeItem.java
+++ app/src/main/java/com/example/quakeviewer/QuakeItem.java
@@ -3,23 +3,27 @@ package com.example.quakeviewer;
 public class QuakeItem {
     private String origin;
     private String location;
-    private String depth;
+    private int depth;
+    private String depthMeasurement;
     private Double latitude;
     private Double longitude;
     private Double magnitude;
 
     public String getOrigin() { return this.origin; }
     public String getLocation() { return this.location; }
-    public String getDepth() { return this.depth; }
+    public int getDepth() { return this.depth; }
+    public String getDepthMeasurement() { return this.depthMeasurement; }
     public Double getLat() { return this.latitude; }
     public Double getLong() { return this.longitude; }
     public Double getMag() { return this.magnitude; }
 
     public QuakeItem(String data, Double latitude, Double longitude) {
         String[] fields = data.split(";");
+        String[] depthInfo = getFieldValue(fields[3]).split(" ");
         this.origin = getFieldValue(fields[0]);
         this.location = getFieldValue(fields[1]);
-        this.depth = getFieldValue(fields[3]);
+        this.depth = Integer.parseInt(depthInfo[0]);
+        this.depthMeasurement = depthInfo[1];
         this.magnitude = Double.valueOf(getFieldValue(fields[4]));
         this.latitude = latitude;
         this.longitude = longitude;
