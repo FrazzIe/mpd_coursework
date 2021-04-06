cf90d99873e645fb01c94a7dffc3c1abd6a8689b
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-04-06 21:03:39 +0100

    add async quake task

--- app/src/main/java/com/example/quakeviewer/QuakeList.java
+++ app/src/main/java/com/example/quakeviewer/QuakeList.java
@@ -1,6 +1,17 @@
 package com.example.quakeviewer;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuakeList {
+    private URL dataSrc;
     private List<QuakeItem> quakes;
     private QuakeItem mostNorthQuake;
     private QuakeItem mostSouthQuake;
@@ -18,7 +29,42 @@ public class QuakeList {
     public QuakeItem getDeepestQuake() { return this.deepestQuake; }
     public QuakeItem getShallowestQuake() { return this.shallowestQuake; }
 
-    public QuakeList() {
-        quakes = new List<QuakeItem>();
+    public QuakeList(URL url) {
+        this.dataSrc = url;
+        this.quakes = new ArrayList<QuakeItem>();
+        this.Refresh();
+    }
+
+    public void Refresh() {
+        QuakeTask quakeTask = new QuakeTask();
+        quakeTask.execute(this.dataSrc);
+    }
+
+    private class QuakeTask extends AsyncTask<URL, Integer, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Boolean doInBackground(URL... urls) {
+            if (urls.length == 0)
+                return false;
+
+            try {
+                URL feed = urls[0];
+                URLConnection feedConnection = feed.openConnection();
+                InputStream feedStream = feedConnection.getInputStream();
+                
+            } catch (IOException e) {
+                Log.e("e", "Error", e);
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            super.onPostExecute(success);
+        }
     }
 }
