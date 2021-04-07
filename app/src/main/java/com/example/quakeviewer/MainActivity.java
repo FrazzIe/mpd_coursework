a5245edaa2d54bdada4a7b17be7aebcb45814011
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-04-07 06:41:54 +0100

    init app with recycler view

--- app/src/main/java/com/example/quakeviewer/MainActivity.java
+++ app/src/main/java/com/example/quakeviewer/MainActivity.java
@@ -1,8 +1,21 @@
 package com.example.quakeviewer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView uiRecyclerView = (RecyclerView) findViewById(R.id.quake_list);
+        RecyclerView.LayoutManager uiRecyclerLayout = new GridLayoutManager(this, 1);
+        uiRecyclerView.setLayoutManager(uiRecyclerLayout);
+
+        Log.d("INFO", "STARTED");
+        URL feed = null;
+
+        try {
+            feed = new URL("http://quakes.bgs.ac.uk/feeds/MhSeismology.xml");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        QuakeList quakeList = new QuakeList(feed, uiRecyclerView);
     }
 }
