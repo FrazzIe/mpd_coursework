649ce35db89e42c81b4c74483719ddeea05661df
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-04-08 04:00:35 +0100

    add filter results page layout

---
+++ app/src/main/java/com/example/quakeviewer/QuakeDateFilter.java
@@ -0,0 +1,14 @@
+package com.example.quakeviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuakeDateFilter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quake_date_filter);
+    }
+}
