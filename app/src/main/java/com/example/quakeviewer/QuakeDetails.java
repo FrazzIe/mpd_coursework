4a448d12861381ae2be21f7e12d4dbe56ebe8be1
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-04-08 02:55:15 +0100

    add quake map view with a marker

--- app/src/main/java/com/example/quakeviewer/QuakeDetails.java
+++ app/src/main/java/com/example/quakeviewer/QuakeDetails.java
@@ -6,10 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class QuakeDetails extends AppCompatActivity {
+    MapView quakeMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +41,49 @@ public class QuakeDetails extends AppCompatActivity {
 
         Date originDate = new Date(origin);
         SimpleDateFormat originFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss");
+        String originDateText = originFormat.format(originDate);
 
-
-        dateText.setText(originFormat.format(originDate));
+        dateText.setText(originDateText);
         locationText.setText(location);
         magnitudeText.setText(magnitude.toString());
         depthText.setText(depth + depthMeasurement);
+
+        LatLng quakeCoords = new LatLng(latitude, longitude);
+        quakeMapView = (MapView) findViewById(R.id.info_map_view);
+        final GoogleMap[] quakeMap = new GoogleMap[1];
+
+        quakeMapView.onCreate(null);
+        quakeMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                quakeMap[0] = googleMap;
+                quakeMap[0].getUiSettings().setZoomControlsEnabled(true);
+                quakeMap[0].getUiSettings().setZoomGesturesEnabled(true);
+                quakeMap[0].getUiSettings().setCompassEnabled(true);
+                quakeMap[0].addMarker(new MarkerOptions().position(quakeCoords).title(originDateText));
+                quakeMap[0].moveCamera(CameraUpdateFactory.newLatLng(quakeCoords));
+            }
+        });
+
+        MapsInitializer.initialize(quakeMapView.getContext());
+    }
+
+
+    @Override
+    public void onResume() {
+        quakeMapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        quakeMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        quakeMapView.onLowMemory();
     }
 }
