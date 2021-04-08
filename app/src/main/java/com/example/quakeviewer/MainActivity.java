fe95f7dd650f134d4c74f5f25a1673c730eba6ef
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-04-09 00:53:33 +0100

    move all logic inside URL try catch block

--- app/src/main/java/com/example/quakeviewer/MainActivity.java
+++ app/src/main/java/com/example/quakeviewer/MainActivity.java
@@ -24,6 +24,7 @@ import java.util.Date;
 // S1916169 - Fraser Watt (Plagiarism check)
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private String quakeFeedUrl = "https://quakes.bgs.ac.uk/feeds/MhSeismology.xml";
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -44,48 +45,54 @@ public class MainActivity extends AppCompatActivity {
         URL feed = null;
 
         try {
-            feed = new URL("https://quakes.bgs.ac.uk/feeds/MhSeismology.xml");
+            //Setup feed URL
+            URL feed = new URL(quakeFeedUrl);
+
+            //Setup date picker
+            Calendar now = Calendar.getInstance();
+            DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(null,
+                    now.get(Calendar.YEAR),
+                    now.get(Calendar.MONTH),
+                    now.get(Calendar.DAY_OF_MONTH)
+            );
+
+            //Initialise Dynamic Earthquake List
+            QuakeList quakeList = new QuakeList(feed, this, datePickerDialog);
+
+            //Setup date picker callback after quakeList is defined
+            //Filter Earthquakes on callback
+            DatePickerDialog.OnDateSetListener datePickerDialogCallback = new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth, int yearEnd, int monthOfYearEnd, int dayOfMonthEnd) {
+                    try {
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("d-M-yyyy HH:mm:ss");
+                        Date fromDate = dateFormat.parse(dayOfMonth + "-" + (++monthOfYear) + "-" + year + " 00:00:00");
+                        Date toDate = dateFormat.parse(dayOfMonthEnd + "-" + (++monthOfYearEnd) + "-" + yearEnd+ " 23:59:59");
+                        quakeList.OpenDateRange(fromDate, toDate);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            datePickerDialog.setOnDateSetListener(datePickerDialogCallback);
+
+            //Add click event listener to open the date picker
+            Button uiFilterBtn = (Button) findViewById(R.id.quake_filter_button);
+            uiFilterBtn.setOnClickListener(view -> {
+                datePickerDialog.show(getFragmentManager(), "datePickerDialog");
+            });
+
+            //Add refresh listener to refresh earthquake list on swipe up
+            SwipeRefreshLayout uiRefreshLayout = findViewById(R.id.quake_swipe_container);
+            uiRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    quakeList.Refresh();
+                }
+            });
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-
-        Calendar now = Calendar.getInstance();
-        DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(null,
-                now.get(Calendar.YEAR),
-                now.get(Calendar.MONTH),
-                now.get(Calendar.DAY_OF_MONTH)
-        );
-
-        QuakeList quakeList = new QuakeList(feed, this, datePickerDialog);
-
-
-        DatePickerDialog.OnDateSetListener datePickerDialogCallback = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth, int yearEnd, int monthOfYearEnd, int dayOfMonthEnd) {
-                try {
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("d-M-yyyy HH:mm:ss");
-                    Date fromDate = dateFormat.parse(dayOfMonth + "-" + (++monthOfYear) + "-" + year + " 00:00:00");
-                    Date toDate = dateFormat.parse(dayOfMonthEnd + "-" + (++monthOfYearEnd) + "-" + yearEnd+ " 23:59:59");
-                    quakeList.OpenDateRange(fromDate, toDate);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        datePickerDialog.setOnDateSetListener(datePickerDialogCallback);
-
-        Button uiFilterBtn = (Button) findViewById(R.id.quake_filter_button);
-        uiFilterBtn.setOnClickListener(view -> {
-            datePickerDialog.show(getFragmentManager(), "datePickerDialog");
-        });
-
-        SwipeRefreshLayout uiRefreshLayout = findViewById(R.id.quake_swipe_container);
-        uiRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                quakeList.Refresh();
-            }
-        });
     }
 }
